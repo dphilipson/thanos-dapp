8685b4259fd5d3c037b48c85e8718be81b98b6b7
David Philipson <david.philipson@gmail.com>
2021-05-05 13:15:04 -0700

    Update Kovan contract address

--- packages/thanos-app/src/util/constants.ts
+++ packages/thanos-app/src/util/constants.ts
@@ -3,7 +3,7 @@ import { EthNetwork } from "./metamask";
 export const IS_PROD = process.env.NODE_ENV === "production";
 
 const HARDHAT_THANOS_ADDRESS = "0x2388d1b9d8e958528643ef6046c638879e210294";
-const KOVAN_THANOS_ADDRESS = "0x3B04075Fe798D78c871Ce87eD30D366670eb36a5";
+const KOVAN_THANOS_ADDRESS = "0x472E27BA0A31562681724eDC7496209e80358fFd";
 
 export function getThanosAddress(network: EthNetwork): string {
   switch (network) {
