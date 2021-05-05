24d23a89794d275fec1c20cb676d1c24842905a7
David Philipson <david.philipson@gmail.com>
2021-05-05 04:25:59 -0700

    Add favicon

--- packages/thanos-app/gatsby-config.js
+++ packages/thanos-app/gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: "thanos-app",
   },
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-react-helmet"],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Thanos Dapp",
+        short_name: "Thanos",
+        start_url: "/",
+        backgroundColor: "#293742",
+        themeColor: "#293742",
+        display: "minimal-ui",
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-theme-ui",
+  ],
 };
